834dd89d9638d60518202a9ef11702500098bbc1
Patryk <patryk.lewandowski.wow@gmail.com>
2026-05-25 00:11:23 +0200

    Configure templates static and media directories

--- config/settings.py
+++ config/settings.py
@@ -64,11 +64,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
-"DIRS": [BASE_DIR / "templates"],
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,7 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
